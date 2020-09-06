c4ffb0e96dcce91340d2cc9edf55cf01eec1d7db
pang453758463 <383354826@qq.com>
2020-09-06 12:01:06 +0800

    Update PluginController.php

--- api/app/Http/Controllers/v1/Admin/PluginController.php
+++ api/app/Http/Controllers/v1/Admin/PluginController.php
@@ -2,21 +2,9 @@
 
 namespace App\Http\Controllers\v1\Admin;
 
-use App\Code;
 use App\common\Plugin;
-use App\Models\v1\Dhl;
-use App\Models\v1\GoodIndent;
-use App\Models\v1\GoodIndentCommodity;
-use App\Models\v1\MoneyLog;
-use App\Models\v1\User;
-use App\Models\v1\UserCoupon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Carbon\Carbon;
-use EasyWeChat\Factory;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Redis;
-use App\common\RedisLock;
 
 class PluginController extends Controller
 {
@@ -40,7 +28,6 @@ class PluginController extends Controller
      */
     public function update($name){
         $Plugin=new Plugin();
-        $autoPlugin=$Plugin->autoPlugin($name);
-        return resReturn(1,$autoPlugin);
+        return $Plugin->autoPlugin($name);
     }
 }
