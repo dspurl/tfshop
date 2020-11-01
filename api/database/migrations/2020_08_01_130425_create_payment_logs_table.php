73fa2240cab6fec76a681e7d29fcd350d83ab2b2
pang453758463 <383354826@qq.com>
2020-11-01 13:42:18 +0800

    去掉数据库迁移更新的代码，之后如果修改数据库的，将在版本更新的时候说明下，而不是添加迁移文件

--- api/database/migrations/2020_08_01_130425_create_payment_logs_table.php
+++ api/database/migrations/2020_08_01_130425_create_payment_logs_table.php
@@ -16,7 +16,6 @@ class CreatePaymentLogsTable extends Migration
     {
         Schema::create('payment_logs', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('user_id')->index()->comment('用户ID');
             $table->string('name',128)->nullable()->comment('订单描述');
             $table->string('number',50)->comment('订单号');
             $table->string('transaction_id',50)->nullable()->comment('支付订单号');
@@ -25,6 +24,8 @@ class CreatePaymentLogsTable extends Migration
             $table->tinyInteger('state')->default(0)->comment('状态0生成1完成2失败');
             $table->bigInteger('pay_id')->default(0)->comment('支付对应的ID');
             $table->string('pay_type',200)->nullable()->comment('支付对应的模型');
+            $table->string('type',80)->nullable()->comment('支付类型标识');
+            $table->string('platform',50)->nullable()->comment('支付平台');
             $table->timestamps();
             $table->charset = 'utf8';
             $table->engine = 'InnoDB';
