11cd68e47aa83158f76d922f8a1c94734d2b8140
pang453758463 <383354826@qq.com>
2021-03-19 19:49:31 +0800

    去掉.nuxt目录

--- api/server.php
+++ api/server.php
@@ -1,5 +1,5 @@
 <?php
-
+ 
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
--- web/.nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['auth'] = require('..\\middleware\\auth.js')
-middleware['auth'] = middleware['auth'].default || middleware['auth']
-
-export default middleware
