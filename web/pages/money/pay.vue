5c12615eb7f044f29dbe07e7a7fe842015ff32ec
dspurl <49799121+dspurl@users.noreply.github.com>
2021-04-09 15:19:13 +0800

    网站支持微信支付、移动端支持微信浏览器支付，待线上测试

--- web/pages/money/pay.vue
+++ web/pages/money/pay.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="box">
     <div class="money-pay">
-      <el-card class="container" shadow="always">
+      <el-card class="container" shadow="always" v-loading="loading">
         <div class="pay-order">
           <div class="el-icon-circle-check"></div>
           <div class="order-info">
             <div class="title">订单提交成功！去付款咯～</div>
             <div class="warn">请在<span>
               <no-ssr>
-                <vue-countdown v-if="list.overtime_time" :time="list.overtime_time * 1000" v-slot="{ days, hours, minutes, seconds }">
+                <vue-countdown @end="getList" v-if="list.overtime_time" :time="list.overtime_time * 1000" v-slot="{ days, hours, minutes, seconds }">
                   {{ days }} 天 {{ hours }} 小时 {{ minutes }} 分 {{ seconds }} 秒
                 </vue-countdown>
               </no-ssr>
@@ -32,29 +32,43 @@
               </ul>
             </div>
             <div class="fright">
-              <div class="total">应付总额：<div class="price"><span>{{list.total| thousands}}</span>元</div></div>
+              <div class="total">应付总额：<div class="price"><span>{{(list.total ? list.total : 0)| thousands}}</span>元</div></div>
               <div class="show-detail" @click="showDetail">订单详情<i class="iconfont dsshop-xia"></i></div>
             </div>
           </div>
         </div>
       </el-card>
-      <el-card class="container mode-payment-box" shadow="always">
+      <el-card class="container mode-payment-box" shadow="always" v-loading="loading">
         <div class="title">选择以下支付方式付款</div>
         <el-divider></el-divider>
         <div class="min-title">支付方式</div>
         <div class="list">
-          <div class="li">
+          <div class="li" v-loading="buttonLoading" @click="payment('weixin')">
             <el-image
               class="image"
-              src="//cdn.cnbj1.fds.api.mi-img.com/mi-mall/c66f98cff8649bd5ba722c2e8067c6ca.jpg"
+              :src="require('assets/img/weixin-pay.jpg')"
               fit="cover"/>
           </div>
-          <div class="li" v-if="list.user" @click="payment(1)">
-            余额支付（{{list.user.money| thousands}}）
+          <div class="li" v-loading="buttonLoading" v-if="list.user" @click="payment(1)">
+            预存款支付（{{list.user.money| thousands}}）
           </div>
         </div>
       </el-card>
     </div>
+    <el-dialog
+        title="微信支付"
+        :visible.sync="centerDialogVisible"
+        :close-on-click-modal="false"
+        width="400px"
+        center>
+        <div class="vue-qrcode">
+          <vue-qrcode :value="qrcode" :options="{ width: 250 }"></vue-qrcode>
+        </div>
+        <div slot="footer" class="dialog-footer">
+          <div>请使用<span class="main-color">微信</span>扫一扫</div>
+          <div>二维码完成支付</div>
+        </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -62,8 +76,9 @@
 import { pay } from '@/api/goodIndent'
 import { unifiedPayment, balancePay } from '@/api/pay'
 import VueCountdown from '@chenfengyuan/vue-countdown';
+import VueQrcode from '@chenfengyuan/vue-qrcode';
 export default {
-  components: { VueCountdown },
+  components: { VueCountdown, VueQrcode },
   layout: 'cart',
   middleware: 'auth',
   head () {
@@ -75,6 +90,10 @@ export default {
     return {
       loading: true,
       detail: false,
+      centerDialogVisible: false,
+      buttonLoading: false,
+      qrcode: '',
+      timer: null,
       list: {}
     }
   },
@@ -85,15 +104,23 @@ export default {
   methods: {
     getList(){
       pay($nuxt.$route.query.id).then(response => {
+        if(response.state !== 1) {  // 订单发生改变时，直接跳转到结果页
+          if (response.state === 4) {
+            // $nuxt.$router.replace('/user/indent/list')
+            $nuxt.$router.replace({path: '/user/indent/detail', query:{id:$nuxt.$route.query.id}})
+          } else {
+            $nuxt.$router.replace('/money/success')
+          }
+
+        }
+        this.loading = false;
         this.list = response
       }).catch(error=>{
         this.$message({
           message: '请求参数有误',
           type: 'error'
         });
-        setTimeout(() => {
-          $nuxt.$router.go(-1)
-        }, 500);
+
       })
     },
     // 显示详情
@@ -102,16 +129,54 @@ export default {
     },
     // 支付
     payment(type){
+      this.buttonLoading = true
       if(type === 1){ // 余额支付
         balancePay({id:$nuxt.$route.query.id}).then(response => {
+          this.buttonLoading = false
           $nuxt.$router.replace('/money/success')
+        })
+      }else{
+        unifiedPayment({
+          id: $nuxt.$route.query.id,
+          platform: type,
+          trade_type: 'NATIVE',
+          type: 'goodsIndent'
+        }).then(response => {
+          this.centerDialogVisible = true
+          this.qrcode = response.code_url
+          this.buttonLoading = false
+          if (this.timer) {
+            clearInterval(this.timer)
+            this.timer = setInterval(() => {
+              this.getList()
+            }, 5000);
+          } else {
+            this.timer = setInterval(() => {
+              this.getList()
+            }, 5000);
+          }
+        }).catch(error=>{
+          this.$message({
+            message: '支付配置有误，请检查',
+            type: 'error'
+          });
         })
       }
     }
+  },
+  destroyed(){
+    clearInterval(this.timer)
   }
 }
 </script>
 <style lang='scss' scoped>
+  .vue-qrcode{
+    text-align: center;
+  }
+  .main-color{
+    margin-left: 5px;
+    margin-right: 5px;
+  }
   .money-pay{
     margin: 40px 0 40px 0;
   }
