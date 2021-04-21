d2008126629cf6be60018231ffca8a84cba300c3
pang453758463 <383354826@qq.com>
2021-04-21 20:32:30 +0800

    命名规范

--- web/middleware/terminal.js
+++ web/middleware/terminal.js
@@ -9,6 +9,6 @@ export default function (context) {
     : navigator.userAgent;
   let flag = context.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
   if(flag){
-    return context.redirect(context.env.h5_URL)
+    return context.redirect(context.env.H5_URL)
   }
 }
