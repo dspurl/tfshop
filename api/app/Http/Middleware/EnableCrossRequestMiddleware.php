5f17c309dfdbecc9c66c196608ac3a455b530b4f
gf123 <root@mchacker.cn>
2021-05-26 00:07:51 +0800

    修复本地调试跨域问题

--- api/app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ api/app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -15,9 +15,10 @@ class EnableCrossRequestMiddleware
      */
     public function handle($request, Closure $next)
     {
+        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
         $resp = $next($request);
         $resp->headers->add([
-            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Origin' => $origin,
             'Access-Control-Allow-Headers' => 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization,applyid,openid,apply-secret,versionid, X-XSRF-TOKEN',
             'Access-Control-Expose-Headers' => 'Authorization, authenticated',
             'Access-Control-Allow-Methods' => 'GET, POST, PATCH, PUT, OPTIONS,DELETE',
