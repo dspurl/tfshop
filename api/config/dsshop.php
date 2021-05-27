a9333373033b9a12c002db68a7d34beb7c03fe20
gf123 <root@mchacker.cn>
2021-05-27 23:10:39 +0800

    修复env键名不对应导致APP_KEY修改无效

--- api/config/dsshop.php
+++ api/config/dsshop.php
@@ -20,7 +20,7 @@ return [
     'homestead' => env('HOMESTEAD_WINDOWS', false),
     'failuretime' => env('VERIFICATION_CODE_FAILURE_TIME', 60), // 验证码失效时间秒，默认60秒
     'versions' => env('API_VERSIONS', 1),   //当前版本
-    'applySecret' => env('PROJECT_KEY', 'base64:szoJ3mSx/5U7zOsJfU7s4pSahiwdh01x6badmz5FtCM='), //API密钥串
+    'applySecret' => env('APP_KEY', 'base64:szoJ3mSx/5U7zOsJfU7s4pSahiwdh01x6badmz5FtCM='), //API密钥串
     'orderOvertime' => env('ORDER_OVERTIME', 1440),  // 订单超时时间(分钟)
     'automaticReceivingState' => env('AUTOMATIC_RECEIVING_STATE', true),  // 是否开启自动收货
     'automaticReceiving' => env('AUTOMATIC_RECEIVING', 7),  // 多少天后自动收货(天)
