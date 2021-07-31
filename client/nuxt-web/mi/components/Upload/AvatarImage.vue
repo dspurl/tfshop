dc41c0512864c531cb894edfb21ae1ce081d077f
pang453758463 <383354826@qq.com>
2021-07-31 11:06:44 +0800

    修复单文件上传组件图片只显示一部分的问题

--- client/nuxt-web/mi/components/Upload/AvatarImage.vue
+++ client/nuxt-web/mi/components/Upload/AvatarImage.vue
@@ -8,7 +8,13 @@
       :show-file-list="false"
       :on-success="handleSuccessList"
       :before-upload="beforeUploadList">
-      <img v-if="file" :src="file" class="avatar">
+      <template v-if="file">
+        <el-image
+          v-if="imgData.type === 1"
+          :src="file"
+          fit="cover"></el-image>
+        <i v-else class="el-icon-folder avatar-uploader-icon"></i>
+      </template>
       <i v-else class="el-icon-plus avatar-uploader-icon"></i>
     </el-upload>
 	</div>
