4aa7a83bd388dece5b5e4bae98246a503c14c079
pang453758463 <383354826@qq.com>
2021-11-15 15:39:22 +0800

    删除web路由，让项目发行时可以缓存路由

--- api/routes/web.php
+++ api/routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 //Auth::routes();
