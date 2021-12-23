d77b93a786c873d6a56d699b62dd47b5f8104824
dspurl <49799121+dspurl@users.noreply.github.com>
2021-12-23 13:59:54 +0800

    撤回版本升级操作
    因重构项目成本过大，故回滚到原来laravel7和vue2版本；
    dsshop继续提供维护，具体维护说明请查看手册
    并开启admin项目的开发，admin项目将使用最新的laravel8和vue3来重构，只做后台，不再涉及多终端；待admin项目成型后，再来重构dsshop

--- admin/vue2/element-admin-v3/config/dev.env.js
+++ admin/vue2/element-admin-v3/config/dev.env.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
   SITE_NAME: '"DSSHOP电商商城"',
-  BASE_API: '"http://demo.test/api/v1/admin/"',
+  BASE_API: '"http://dsshop.test/api/v1/admin/"',
 }
