77853df6b91ae2c2f26acf967f64cbc569fd0169
pang453758463 <383354826@qq.com>
2023-04-07 15:36:20 +0800

    优化多图上传组件，支持图片预览

--- admin/vue2/element-admin-v3/src/components/Upload/InsertImage.vue
+++ admin/vue2/element-admin-v3/src/components/Upload/InsertImage.vue
@@ -6,6 +6,7 @@
       :headers="imgHeaders"
       :on-success="handleSuccessList"
       :on-remove="handleRemove"
+      :on-preview="handlePictureCardPreview"
       :before-upload="beforeUploadList"
       :on-error="handleError"
       :data="imgData"
@@ -15,6 +16,9 @@
       list-type="picture-card">
       <i slot="default" class="el-icon-plus"/>
     </el-upload>
+    <el-dialog :visible.sync="dialogVisible" modal-append-to-body append-to-body>
+      <img :src="dialogImageUrl" width="100%" alt="">
+    </el-dialog>
   </div>
 </template>
 
